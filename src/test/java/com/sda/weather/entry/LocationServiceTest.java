08c97a662de6e41afb768e55ef2b1a2d3333d8b2
Michal Paukszto <michal.paukszto@gmail.com>
2022-04-02 15:22:30 +0200

    review

--- src/test/java/com/sda/weather/entry/LocationServiceTest.java
+++ src/test/java/com/sda/weather/entry/LocationServiceTest.java
@@ -1,13 +1,11 @@
 package com.sda.weather.entry;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jdk.dynalink.linker.support.Guards;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.assertj.core.api.Assertions.catchThrowable;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 public class LocationServiceTest {
 
@@ -18,16 +16,16 @@ public class LocationServiceTest {
         LocationRepository locationRepository = new LocationRepositoryMock();
         ObjectMapper objectMapper = new ObjectMapper();
         locationService = new LocationService(locationRepository, objectMapper);
-
     }
 
     @Test
-    void createLocation_whenDataIsProper_returnsNewObject(){
+    void createLocation_whenDataIsProper_returnsNewObject() {
         //when
         Location result = locationService.createLocation("Lodz", "Polska");
         //then
-        assertThat(result).isNotNull;
+        assertThat(result).isNotNull();
     }
+
     @Test
     void createLocation_whenLocationIsEmpty_throwsAnException() {
         // when
@@ -35,8 +33,4 @@ public class LocationServiceTest {
         // then
         assertThat(result).isExactlyInstanceOf(IllegalArgumentException.class);
     }
-
-
-
-
 }
