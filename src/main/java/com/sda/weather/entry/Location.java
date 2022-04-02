6c9d73b58b5eafc9f589ef0be59ae74f6db15ab6
KarJar07 <jarek.karol1@gmail.com>
2022-04-02 12:57:02 +0200

    add location

---
+++ src/main/java/com/sda/weather/entry/Location.java
@@ -0,0 +1,30 @@
+package com.sda.weather.entry;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table (name = "location")
+
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String city;
+    private String country;
+    private Double longitude;
+    private Double latitude;
+
+    public Location(long id, String city, String country, Double longitude, Double latitude) {
+        this.id = id;
+        this.city = city;
+        this.country = country;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+}
