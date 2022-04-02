03529dfcbeaf251728aa0228a3763fd7ee172442
KarJar07 <jarek.karol1@gmail.com>
2022-04-02 12:29:04 +0200

    user interface done

--- src/main/java/com/sda/weather/WeatherApplication.java
+++ src/main/java/com/sda/weather/WeatherApplication.java
@@ -1,8 +1,36 @@
 package com.sda.weather;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sda.weather.entry.LocationController;
+import com.sda.weather.entry.LocationRepository;
+import com.sda.weather.entry.LocationRepositroyImpl;
+import com.sda.weather.entry.LocationService;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class WeatherApplication {
+//    public static void main(String[] args) {
+//        System.out.println("Hello World ");
+
     public static void main(String[] args) {
-        System.out.println("Hello World ");
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+
+        SessionFactory sessionFactory = new MetadataSources(registry)
+                .buildMetadata()
+                .buildSessionFactory();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        LocationRepository locationRepository = new LocationRepositroyImpl(sessionFactory);
+        LocationService locationService = new LocationService (locationRepository, objectMapper);
+        LocationController locationController = new LocationController (objectMapper, locationService);
+        UserInterface userInterface = new UserInterface(locationController);
+
+        userInterface.run();
+    }
 
     }
-}
+
